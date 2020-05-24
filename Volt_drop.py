e357344945cf7654e322b5e16cd2fc97a549d30d
Aaron McDonnell <55592619+AaronMcD43@users.noreply.github.com>
2020-05-24 13:15:07 +0100

    Add files via upload
    In this revision I added in an input section for the power system parameters. I added in a Try and Except. I also improved the output section by adding strings and units for the results, with the results being to two decimal places.

--- Volt_drop.py
+++ Volt_drop.py
@@ -1,13 +1,23 @@
-#This program calculates the sending end voltage from a system.
+# This program calculates the sending end voltage from a system.
 import math
 import cmath
 import numpy as np
 
-VS_L_L = 11000  # Sending End Voltage Level
+try:
+    # 11000  # Sending End Voltage Level
+    VS_L_L = int(
+        input("Please Enter the Sending End Voltage in units of volts: "))
+    # 400# Recieving End Voltage Level
+    VR_L_L = int(
+        input("Please Enter the Recieving End Voltage in units of volts: "))
+    # 11000  # the current and impedance at this voltage level
+    Vref = int(input(
+        "Please Enter the Voltage at which the current and impedance will be refeered to in units of volts: "))
+except ValueError as e:
+    print("Error reading inputs: %s" % e)
+
 VS_Ph = VS_L_L/math.sqrt(3)
-VR_L_L = 400  # Recieving End Voltage Level
 VR_Ph = VR_L_L / math.sqrt(3)
-Vref = 11000  # the current and impedance at this voltage level
 
 
 class Transformer():
@@ -48,34 +58,32 @@ class Load():
         return self.I
 
 
-#Section were you create you create your system components
+# Section were you create you create your system components
 Transformer_1 = Transformer(0, 2, VS_L_L, VR_L_L)
 cable_1 = Cable(3, 7, 1, VS_L_L)
 cable_2 = Cable(0.01, 0.005, 1, VR_L_L)
 Load_1 = Load(250000, 1, 400)
 
-#Calculated the impedance of the system for the reference voltage Vref (set as the variable above)
+# Calculated the impedance of the system for the reference voltage Vref (set as the variable above)
 z1 = cable_1.Total_Z() * (Vref/cable_1.V)**2
 z2 = Transformer_1.Z * (Vref/Transformer_1.HV)**2
 z3 = cable_2.Total_Z() * (Vref/cable_2.V)**2
 Zt = z1 + z2 + z3  # Total Z in the system at the reference voltage
 
-#Calculate the Load currents
+# Calculate the Load currents
 I_400 = Load_1.Calc_I()  # Load current at the voltage the load is connected to
 # load current seen from the HV of the network
 I_11000 = Load_1.Calc_I()*(Load_1.V_L_L/VS_L_L)
 
-#Calculate the Voltage Drops
+# Calculate the Voltage Drops
 IRZT = I_11000*(Zt)
 Act_VS_Ph = math.sqrt((VS_Ph + IRZT.real)**2 + IRZT.imag**2)
+# actual Sending End Voltage with that load and system of impedances
 Act_VS_L_L = Act_VS_Ph*math.sqrt(3)
 volt_diff = Act_VS_L_L - VS_L_L
 
+print("The Load current measured at the load is {:.2f} A".format(I_400))
+print("The Load current measured on the HV side is {:.2f} A".format(I_11000))
+print("The Impedance of system with reference to Vref {:.2f} Ohms".format(Zt))
+print("The Voltage drop of the system {:.2f} V".format(volt_diff))
 
-print(I_400)  # Load current at the voltage the load is connected to
-print(I_11000)  # load current flowing from the HV of the network
-print(Zt)  # impedance @11 kV which is the reference voltage Vref
-# actual Sending End Voltage with that load and system of impedances
-print(Act_VS_L_L)
-print(volt_diff)  # Differnce from the reference voltage of 11kV
- 
